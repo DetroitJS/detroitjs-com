342d86e4996e02cc307767d4c2f0c9acb2b0e3e7
Dennay Bedard <dennay.marie@gmail.com>
2017-04-06 21:35:19 -0400

    top padding added on the header and horizontal padding added to code of conduct

--- pages/code-of-conduct.js
+++ pages/code-of-conduct.js
@@ -1,31 +1,31 @@
 import Layout from '../components/layout'
 export default () => (
   <Layout>
-    <div className="measure lh-copy center">
-    <h1>Welcome to DetroitJS</h1>
-    <h2 id="codeofconduct">Code of Conduct</h2>
+    <div className="measure lh-copy center ph3">
+        <h1>Welcome to DetroitJS</h1>
+        <h2 id="codeofconduct">Code of Conduct</h2>
 
-    <p>All delegates, speakers, sponsors and volunteers at any DetroitJS event are required to agree with the following code of conduct. Organizers will enforce this code throughout the event.</p>
+        <p>All delegates, speakers, sponsors and volunteers at any DetroitJS event are required to agree with the following code of conduct. Organizers will enforce this code throughout the event.</p>
 
-    <h3 id="thequickversion">The Quick Version</h3>
+        <h3 id="thequickversion">The Quick Version</h3>
 
-    <p>DetroitJS is dedicated to providing a harassment-free experience for everyone, regardless of gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, religion (or lack thereof), or technology choices. We do not tolerate harassment of DetroitJS participants in any form. Sexual language and imagery is not appropriate for any DetroitJS venue, including talks, workshops, parties, Twitter and other online media. DetroitJS participants violating these rules may be sanctioned or expelled from the DetroitJS without a refund at the discretion of the DetroitJS organizers.</p>
+        <p>DetroitJS is dedicated to providing a harassment-free experience for everyone, regardless of gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, religion (or lack thereof), or technology choices. We do not tolerate harassment of DetroitJS participants in any form. Sexual language and imagery is not appropriate for any DetroitJS venue, including talks, workshops, parties, Twitter and other online media. DetroitJS participants violating these rules may be sanctioned or expelled from the DetroitJS without a refund at the discretion of the DetroitJS organizers.</p>
 
-    <h3 id="thelessquickversion">The Less Quick Version</h3>
+        <h3 id="thelessquickversion">The Less Quick Version</h3>
 
-    <p>Harassment includes offensive verbal comments related to gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, religion, technology choices, sexual images in public spaces, deliberate intimidation, stalking, following, harassing photography or recording, sustained disruption of talks or other events, inappropriate physical contact, and unwelcome sexual attention.</p>
+        <p>Harassment includes offensive verbal comments related to gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, religion, technology choices, sexual images in public spaces, deliberate intimidation, stalking, following, harassing photography or recording, sustained disruption of talks or other events, inappropriate physical contact, and unwelcome sexual attention.</p>
 
-    <p>Participants asked to stop any harassing behavior are expected to comply immediately.</p>
+        <p>Participants asked to stop any harassing behavior are expected to comply immediately.</p>
 
-    <p>Sponsors are also subject to the anti-harassment policy. In particular, sponsors should not use sexualized images, activities, or other material. Booth staff (including volunteers) should not use sexualized clothing/uniforms/costumes, or otherwise create a sexualized environment.</p>
+        <p>Sponsors are also subject to the anti-harassment policy. In particular, sponsors should not use sexualized images, activities, or other material. Booth staff (including volunteers) should not use sexualized clothing/uniforms/costumes, or otherwise create a sexualized environment.</p>
 
-    <p>If a participant engages in harassing behavior, the DetroitJS organizers may take any action they deem appropriate, including warning the offender or expulsion from the DetroitJS.</p>
+        <p>If a participant engages in harassing behavior, the DetroitJS organizers may take any action they deem appropriate, including warning the offender or expulsion from the DetroitJS.</p>
 
-    <p>If you are being harassed, notice that someone else is being harassed, or have any other concerns, please contact a member of DetroitJS immediately.</p>
+        <p>If you are being harassed, notice that someone else is being harassed, or have any other concerns, please contact a member of DetroitJS immediately.</p>
 
-    <p>We expect participants to follow these rules at all DetroitJS meetups and DetroitJS-related social endeavors.</p>
+        <p>We expect participants to follow these rules at all DetroitJS meetups and DetroitJS-related social endeavors.</p>
 
-    <p>Organizers: <a href="https://twitter.com/seafoam6" style={{color:'black'}}>Steve</a>, <a href="https://twitter.com/SIXLIV3" style={{color:'black'}}>TJ</a></p>
+        <p>Organizers: <a href="https://twitter.com/seafoam6" style={{color:'black'}}>Steve</a>, <a href="https://twitter.com/SIXLIV3" style={{color:'black'}}>TJ</a></p>
     </div>
   </Layout>
 )
