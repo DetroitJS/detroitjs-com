27471f76cb752da4a2334f58143312aa2a234897
Steve Barman <steve@stevebarman.com>
2017-11-04 19:24:20 -0400

    adds sass and necessary config for independent style sheets

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+
+const path = require('path')
+const glob = require('glob')
+
 module.exports = {
   webpack: (config, { dev }) => {
     if (!dev) {
@@ -11,7 +15,34 @@ module.exports = {
         exclude: /node_modules/
     };
     rules.push(lintConfig);
-    
+    config.module.rules.push(
+      {
+        test: /\.(css|scss)/,
+        loader: 'emit-file-loader',
+        options: {
+          name: 'dist/[path][name].[ext]'
+        }
+      }
+    ,
+      {
+        test: /\.css$/,
+        use: ['babel-loader', 'raw-loader', 'postcss-loader']
+      }
+    ,
+      {
+        test: /\.s(a|c)ss$/,
+        use: ['babel-loader', 'raw-loader', 'postcss-loader',
+          { loader: 'sass-loader',
+            options: {
+              includePaths: ['styles', 'node_modules']
+                .map((d) => path.join(__dirname, d))
+                .map((g) => glob.sync(g))
+                .reduce((a, c) => a.concat(c), [])
+            }
+          }
+        ]
+      }
+    )
     return config
   }
 }
