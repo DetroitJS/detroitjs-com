2db5dce4ccc9cadbbb92d178b7805df3d908fc8c
Steve Barman <steve@stevebarman.com>
2017-11-04 23:19:13 -0400

    removes unneeded code

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import NavLink from './navLink.js'
-import sty from '../styles/index.scss'
 
 const Footer = () => (
   <footer className="pv4 ph3 ph5-m ph6-l mid-gray bg">
     <div className="tc mt3">
       <NavLink />
     </div>
-    <style dangerouslySetInnerHTML={{ __html: sty }} />
   </footer>
 )
 
